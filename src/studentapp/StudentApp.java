882ea8deab60c11de1bec65029a67e0bfd2dda18
Praeee <60050211@kmitl.ac.th>
2020-08-19 17:10:55 +0700

    add getter setter lastname

--- src/studentapp/StudentApp.java
+++ src/studentapp/StudentApp.java
@@ -15,8 +15,8 @@ public class StudentApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Student student1 = new Student(12345 , "prae pornpailin");
-        System.out.println(student1.getId() +" "+ student1.getName());
+        Student student1 = new Student(12345 , "prae", "pornpailin");
+        System.out.println(student1.getId() +" "+ student1.getName() +" "+student1.getLastname());
     }
     
 }
