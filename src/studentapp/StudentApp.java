f645bd1a4af3ea9a2dd178e6032be48ac20c0d86
Praeee <43135341+Praeee@users.noreply.github.com>
2020-08-19 17:00:08 +0700

    Add files via upload

---
+++ src/studentapp/StudentApp.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentapp;
+
+/**
+ *
+ * @author pjmbt
+ */
+public class StudentApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Student student1 = new Student(12345 , "prae pornpailin");
+        System.out.println(student1.getId() +" "+ student1.getName());
+    }
+    
+}
